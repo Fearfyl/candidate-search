1d0ca332b3c63a6dace6ce919203dbd649407182
Fearfyl <wadebuchanan18@gmail.com>
2025-02-25 12:59:23 -0500

    Implement navigation bar and add CandidateCard component

---
+++ Develop/src/components/CandidateCard.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Candidate from '../interfaces/Candidate.interface';
+import '../styles/CandidateCard.css';
+
+interface CandidateCardProps {
+    candidate: Candidate;
+}
+
+const CandidateCard: React.FC<CandidateCardProps> = ({ candidate }) => (
+    <li className="candidate-card" key={candidate.id}>
+        <img src={candidate.avatar_url} alt={candidate.login} width="50" />
+        <a href={candidate.html_url} target="_blank" rel="noopener noreferrer">
+            {candidate.login}
+        </a>
+        <p>Name: {candidate.name || 'user has not added that info'}</p>
+        <p>Bio: {candidate.bio || 'user has not added that info'}</p>
+        <p>Location: {candidate.location || 'user has not added that info'}</p>
+        <p>Email: {candidate.email || 'user has not added that info'}</p>
+        <p>Company: {candidate.company || 'user has not added that info'}</p>
+    </li>
+);
+
+export default CandidateCard;
--- Develop/src/components/Nav.tsx
+++ Develop/src/components/Nav.tsx
@@ -1,8 +1,17 @@
-const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
-  return (
-    <div>Nav</div>
-  )
-};
+import { Link } from 'react-router-dom';
+import '../styles/Nav.css';
+
+const Nav = () => (
+  <nav className="nav">
+    <ul>
+      <li className="nav-item">
+        <Link to="/" className="nav-link">Home</Link>
+      </li>
+      <li className="nav-item">
+        <Link to="/SavedCandidates" className="nav-link">Potential Candidates</Link>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default Nav;
